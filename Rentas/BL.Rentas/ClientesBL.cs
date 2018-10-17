93e3448769edcf651ff2574302d6632803140654
ronyflores <ronless351@gmail.com>
2018-10-16 22:41:39 -0600

    actualizado

--- Rentas/BL.Rentas/ClientesBL.cs
+++ Rentas/BL.Rentas/ClientesBL.cs
@@ -17,7 +17,7 @@ namespace BL.Rentas
             var cliente1 = new Cliente();
             cliente1.Id = 1;
             cliente1.Nombre = "Bruno Jose Diaz";
-            cliente1.Telefono = 98786534;
+            cliente1.Telefono = "98786534";
             cliente1.Correo = "Bruno_Diaz@hotmail.com";
             cliente1.Direccion = "Col.Rivera Hernandes,Casa 2345,Calle 24 y 3 avenida";
             cliente1.Activo = true;
@@ -27,7 +27,7 @@ namespace BL.Rentas
             var cliente2 = new Cliente();
             cliente2.Id = 2;
             cliente2.Nombre = "Kevin Mitnick";
-            cliente2.Telefono = 513456789;
+            cliente2.Telefono = "513456789";
             cliente2.Correo = "kevin_hack1@hotmail.com";
             cliente2.Direccion = "California";
             cliente2.Activo = true;
@@ -37,7 +37,7 @@ namespace BL.Rentas
             var cliente3 = new Cliente();
             cliente3.Id = 3;
             cliente3.Nombre = "Jonathan James";
-            cliente3.Telefono = 518459604;
+            cliente3.Telefono = "518459604";
             cliente3.Correo = "Jonathan_c0mrade@hotmail.com";
             cliente3.Direccion = "Col.Guadalupe ";
             cliente3.Activo = true;
@@ -47,7 +47,7 @@ namespace BL.Rentas
             var cliente4 = new Cliente();
             cliente4.Id = 4;
             cliente4.Nombre = "Adrian Lamo";
-            cliente4.Telefono = 511234567;
+            cliente4.Telefono = "511234567";
             cliente4.Correo = "Adrian_Hackersinhogar@hotmail.com";
             cliente4.Direccion = "Las Vegas ";
             cliente4.Activo = true;
@@ -57,7 +57,7 @@ namespace BL.Rentas
             var cliente5 = new Cliente();
             cliente5.Id = 5;
             cliente5.Nombre = "George Hotz";
-            cliente5.Telefono = 511235277;
+            cliente5.Telefono = "511235277";
             cliente5.Correo = "George_geohot@hotmail.com";
             cliente5.Direccion = "Col.Carmen";
             cliente5.Activo = true;
@@ -128,13 +128,21 @@ namespace BL.Rentas
                 resultado.Exitoso = false;
             }
 
+            if (string.IsNullOrEmpty(cliente.Telefono) == true)
+            {
+                resultado.Mensaje = "Ingrese el numero de Telefono";
+                resultado.Exitoso = false;
+            }
+
             if (string.IsNullOrEmpty(cliente.Correo) == true)
             {
                 resultado.Mensaje = "Ingrese el Correo electronico";
                 resultado.Exitoso = false;
             }
-                       
-             return resultado;
+
+            
+
+            return resultado;
         }
 
     }
@@ -143,7 +151,7 @@ namespace BL.Rentas
         public int Id { get; set; }
         public string Nombre  { get; set; }
         public string Correo { get; set; }
-        public int Telefono { get; set; }
+        public string Telefono { get; set; }
         public string Direccion { get; set; }
         public bool Activo { get; set; }
 
